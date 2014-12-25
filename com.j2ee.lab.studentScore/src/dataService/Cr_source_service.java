e350b1c0f552e26b9a50d38ea84cd3a09c912f2b
wsy121250155 <314784088@qq.com>
2014-12-25 21:11:47 +0800

    。

--- com.j2ee.lab.studentScore/src/dataService/Cr_source_service.java
+++
@@ -1,9 +0,0 @@
-package dataService;
-
-import java.util.List;
-
-import data.CourseRecord;
-
-public interface Cr_source_service {
-	public List<CourseRecord> getCourseRecords(int sid);
-}
--- com.j2ee.lab.studentScore/src/dataService/Stu_source_service.java
+++
@@ -1,7 +0,0 @@
-package dataService;
-
-import data.Student;
-
-public interface Stu_source_service {
-	public Student getStudent(int sid);
-}
